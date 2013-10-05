2f71efbd16089c57e743540f824ce2e7f86a1481
Luc St-Louis <lucs@pobox.com>
2013-10-05 16:53:26 -0400

    Manual.pm improved.

--- lib/Vim/Debug/Manual.pm
+++ lib/Vim/Debug/Manual.pm
@@ -2,60 +2,56 @@
 package Vim::Debug::Manual;
 # VERSION
 
-=head1 What is VimDebug?
+=head1 What is VimDebug
 
-VimDebug integrates the Perl debugger with Vim, allowing developers to
-visually step through their code, examine variables, set or clear
-breakpoints, etc.
+VimDebug allows you to launch the Perl debugger (*1) on some file that
+you have opened in Vim and to visually step through your code, set
+breakpoints, examine variables, etc.
 
-VimDebug is known to work under Unix/Ubuntu/OSX. It requires Perl 5.FIXME or
-later and some CPAN modules may need to be installed.  It also requires Vim
-7.FIXME or later that was built with the +signs and +perl extensions.
+VimDebug is known to work under Unix/Ubuntu/OSX. It is implemented as
+a Perl module, Vim::Debug, that comes with some Vim add-on files.
 
-=head1 How do I install VimDebug?
+=head1 Installation requirements
 
-VimDebug has a Perl component and a Vim component. The Vim component
-happens to be supplied with the Perl component.
+You will need a Vim 7.3 or later that was built with at least the
++perl, +signs, and +autoload extensions.
 
-The Perl component, Vim::Debug, can be obtained from CPAN. A simple
-way to install it is to use L<cpanminus's|https://metacpan.org/module/App::cpanminus> C<cpanm>
-program. First, install C<cpanminus>:
+The compiled in +perl extension will be used to run the Vim::Debug
+files and its dependencies, so make make sure to use this perl when
+installing Vim::Debug (you may need to rebuild your Vim to match your
+perl if this is not the case).
 
-    curl -L http://cpanmin.us | perl - --sudo App::cpanminus
+=head1 Installing VimDebug
 
-Then, install VimDebug's Perl files:
+First, install the Vim::Debug module and its dependencies from CPAN.
+If you're unsure how to do this, it may be as simple as C<cpanm
+Vim::Debug> if you first install
+L<cpanminus|https://metacpan.org/module/App::cpanminus>.
 
-    cpanm Vim::Debug
-
-To install the Vim component, execute the following program,
-which was installed with the Perl component:
+Vim::Debug comes with some Vim add-on files. It also comes with a
+program you should run to install these files and build the help tags
+file. For example:
 
     vimdebug-install -d ~/.vim
 
-You may want to replace C<~/.vim> by some other directory that your
-Vim recognizes as a runtimepath directory. See Vim's C<:help
+You may want to replace C<~/.vim> with some other directory that your
+Vim will recognize as a runtimepath directory. See Vim's C<:help
 'runtimepath'> for more information.
 
-Finally, launch Vim and install and read the Vim help file, which
-describes how to use VimDebug:
+Again, note that the perl that will be executing that command should
+be the same as the one used for the Vim instance's +perl extension,
+otherwise, you may end up using the wrong version of VimDebug's Vim
+add-ons.
 
-    :helptags ~/.vim/doc
-    :help VimDebug
-
-Make sure that the directory where that C<doc> directory resides is in
-your Vim runtimepath, else Vim won't find its help information even if
-it manages to build the help index.
+Once installed, launch Vim and read ':help VimDebug'.
 
 =head1 Improving VimDebug
 
-VimDebug is on github: https://github.com/kablamo/VimDebug.git
+Read the git repo's ./documentation/DEVELOPER file, repo which you can
+clone from L<https://github.com/kablamo/VimDebug.git>.
 
-To do development work on VimDebug, clone its git repo and read
-./documentation/DEVELOPER.
-
-In principle, the VimDebug code can be extended to handle other
-debuggers, like the one for Ruby or Python, but that remains to be
-done.
+(*1) In principle, the VimDebug code can be extended to handle other
+debuggers, like Ruby's or Python's, but that remains to be done.
 
 Please note that this code is in beta.
 
